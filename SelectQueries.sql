9f32a41a8a12b5ce848fb3df13c42fbd0bf05923
Nicholas Abbott <Nicholas.Abbott.BSEE@gmail.com>
2022-06-23 11:19:53 -0500

    4 SELECT queries completed

---
+++ SelectQueries.sql
@@ -0,0 +1,18 @@
+Use OrderManagement;
+
+SELECT CustomerId, FirstName, LastName, CustAddress
+	FROM Customer
+	WHERE FirstName = 'Nick'
+
+SELECT *
+	FROM Customer
+	WHERE LastName LIKE '%Lear%'
+
+SELECT FirstName, BirthDate
+	FROM Customer
+	WHERE DATEDIFF(year, BirthDate, GETDATE()) >= 25
+
+SELECT FirstName, LastName
+	FROM Customer
+	WHERE DATEDIFF(year, BirthDate, GETDATE()) >= 25 
+	AND DATEDIFF(year, BirthDate, GETDATE()) <= 45
